d49e5d86a735e98fa9c3dec92670931e63bb0c09
StarukhDaniil <starukh.danil@gmail.com>
2025-09-24 01:21:51 +0300

    ESP32 gets information from STM32 via UART2 now
    ESP32 gets information and prints it on LCD for debugging

--- Kursova_2nd_grade_esp32.ino
+++ Kursova_2nd_grade_esp32.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+// #include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+HardwareSerial uart(2);
+
+#define UART2_BAUD 115200
+#define UART2_RX 16
+#define UART2_TX 17
+
+uint16_t uart_value = 0;
+
+void setup() {
+  uart.begin(UART2_BAUD, SERIAL_8N1, UART2_RX, UART2_TX);
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print("");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (uart.available()) {
+    uart.readBytes((uint8_t*)(&uart_value), 2);
+    lcd.setCursor(0, 0);
+    lcd.print(String(uart_value) + "    ");
+  }
 }
