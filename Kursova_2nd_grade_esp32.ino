64ea513458a81ea88e4bc6ef6c0547212f9938f7
StarukhDaniil <starukh.danil@gmail.com>
2025-09-24 23:28:53 +0300

    Changes in LCD
    It is now swapping bytes to get the right number and prints both
    varients for debugging. Also counter is added to LCD

--- Kursova_2nd_grade_esp32.ino
+++ Kursova_2nd_grade_esp32.ino
@@ -8,21 +8,35 @@ HardwareSerial uart(2);
 #define UART2_RX 16
 #define UART2_TX 17
 
-uint16_t uart_value = 0;
+uint16_t ADC_value = 0;
+uint16_t counter = 0;
+
+uint16_t a = 0;
 
 void setup() {
   uart.begin(UART2_BAUD, SERIAL_8N1, UART2_RX, UART2_TX);
+  uart.setRxBufferSize(2);
   lcd.init();
   lcd.backlight();
-
-  lcd.setCursor(0, 0);
-  lcd.print("");
+  lcd.setCursor(0, 3);
 }
 
 void loop() {
-  if (uart.available()) {
-    uart.readBytes((uint8_t*)(&uart_value), 2);
+  if (uart.available() > 0) {
+    uart.readBytes((uint8_t*)(&ADC_value), sizeof(ADC_value));
+    ++counter;
+
+    a = ADC_value >> 8;
+    a |= ((ADC_value & 0x00ff) << 8);
+
     lcd.setCursor(0, 0);
-    lcd.print(String(uart_value) + "    ");
+    lcd.print(String(ADC_value) + "    ");
+    lcd.setCursor(10, 0);
+    lcd.print(String(a) + "    ");
+    lcd.setCursor(0, 1);
+    lcd.print(String(counter) + "    ");
+    while(uart.available()) {
+      uart.read();
+    }
   }
 }
